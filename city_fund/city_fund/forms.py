3658d6d4471cc34e3910308ef2526cafa3b5f497
KBratland <kristin.bratland@gmail.com>
2014-12-11 10:58:41 -0800

    December 11th, 2014 #1

--- city_fund/city_fund/forms.py
+++ city_fund/city_fund/forms.py
@@ -8,6 +8,14 @@ class SignUpForm(ModelForm):
         model = SignUp
         fields = ['name', 'email', 'message']
 
+    def unique_email(self):
+        try:
+            user = SignUp.objects.get(email__iexact=self.cleaned_data['email'])
+        except SignUp.DoesNotExist:
+            return self.cleaned_data['email']
+        raise forms.ValidationError(_(u'This email address is already in use. Please supply a different email address.'))
+
+
 
 # class SignUp(forms.Form):
 #     name = forms.CharField(label="Your Name", max_length=100)
